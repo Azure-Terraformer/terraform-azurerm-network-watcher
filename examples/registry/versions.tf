bdee9ebb22c15d116ca31e73d99d73ea408c92f6
Mark Tinderholt <markti@outlook.com>
2026-03-21 06:35:41 -0400

    ftest concurrency

--- examples/registry/versions.tf
+++ examples/registry/versions.tf
@@ -2,3 +2,16 @@ provider "azurerm" {
   features {}
   subscription_id = "b2dcbc85-4327-454d-8972-b8ab7fcf5e76"
 }
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.38.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.7.2"
+    }
+  }
+}
